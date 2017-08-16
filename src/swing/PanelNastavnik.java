98fd14dae7e32003e95b5749a7f43701dc5c8ba3
nklapic <nklapic@NBTNO159.dccsintra.net>
2017-08-16 19:17:37 +0200

    Initial Panel nastavnik commit.

--- src/swing/PanelNastavnik.java
+++ src/swing/PanelNastavnik.java
@@ -1,19 +1,35 @@
 package swing;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 import jpa.Nastavnik;
+import swing.knjigaPanels.KnjigaPregled;
 
 public class PanelNastavnik extends JFrame {
 	public PanelNastavnik (Nastavnik n){
 		this.nastavnik = n;
-		add((new JPanel()).add(new JLabel("Hello from nastavnik dashboard")));
 		setTitle(n.getKorisnik().getImeKorisnika() + " " + n.getKorisnik().getPrezimeKorisnika());
+		
+		JPanel panel = new JPanel();
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu knjiga = new JMenu("Books");
+		knjiga.add(knjigaPregled.getMenuItem(panel));
+		
+		menuBar.add(knjiga);
+		
+		setJMenuBar(menuBar);
+		
+		add(panel);
+		
+		
+		
+		
 	}
 	private Nastavnik nastavnik;
+	private KnjigaPregled knjigaPregled = new KnjigaPregled(false);
 }
