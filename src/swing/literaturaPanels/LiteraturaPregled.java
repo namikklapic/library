0db9563c8537ce1f749a79d0d59b5a00a75cedf5
nklapic <nklapic@NBTNO159.dccsintra.net>
2017-08-26 19:25:25 +0200

    Handled exception when there is no Predmet in database.

--- src/swing/literaturaPanels/LiteraturaPregled.java
+++ src/swing/literaturaPanels/LiteraturaPregled.java
@@ -8,6 +8,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -68,8 +69,13 @@ public class LiteraturaPregled extends JFrame {
 	}
 
 	public void prikazi() {
-		setVisible(true);
+		
 		List<Predmet> predmeti = nastavnikPredmetServiceBean.getPredmetiByNastavnik(this.nastavnik);
+		if(predmeti == null) {
+			JOptionPane.showMessageDialog(this, "You must first add some Predmets", "No Predmets", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		setVisible(true);
 		this.currentPredmet = predmeti.get(0);
 		for (Predmet p : predmeti) {
 			this.cbPredmeti.addItem(p);
